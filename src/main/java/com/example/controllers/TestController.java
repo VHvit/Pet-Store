7aa9a56b72a26bc7b146ed06be6936f5e0e9bf92
VHvit <vithvit3009@gmail.com>
2024-01-29 20:00:42 +0300

    hmhm

---
+++ src/main/java/com/example/controllers/TestController.java
@@ -0,0 +1,34 @@
+package com.example.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping("/unsecured")
+    public String unsecuredData() {
+        return "Unsecured data";
+    }
+
+    @GetMapping("/secured")
+    public String securedData() {
+        return "Secured data";
+    }
+
+    @GetMapping("/admin")
+    public String adminData() {
+        return "Admin data";
+    }
+
+    @GetMapping("/info")
+    public String userData(Principal principal) {
+        return principal.getName();
+    }
+}
